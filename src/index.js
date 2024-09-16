4a0291ae12550d8ee9df0890f3ab8a1cb1b1626a
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2024-09-16 14:23:44 +0200

    add new components + css

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.css';
-import Nav from './components/nav/nav';
-import Footer from './components/footer/footer';
-import Home from './pages/home/home';
-import About from './pages/about/about';
+import Nav     from './components/nav/nav';
+import Home    from './pages/home/home';
+import About   from './pages/about/about';
+import Housing from './pages/housing/housing';
+import Error   from './pages/error/error';
+import Footer  from './components/footer/footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,6 +20,8 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />      
+        <Route path="/housing/:id" element={<Housing />} />
+        <Route path="*" element={<Error />} />
         </Routes>
 
       <Footer />
---
+++ src/pages/housing/housing.jsx
@@ -0,0 +1,43 @@
+import { useParams } from 'react-router-dom';
+import data from "../../assets/data.json";
+import Collapse from "../../components/collapse/collapse";
+import Error from "../../pages/error/error";
+import HostInfo from '../../components/hostinfo/hostinfo';
+import Tags from '../../components/tags/tags';
+import Slideshow from '../../components/slideshow/slideshow';
+import Rating from '../../components/rating/rating';
+
+import "./housing.css";
+
+
+function Housing() {
+    const { id } = useParams();
+    const card = data.find((card) => card.id === id);
+
+    if (!card) {
+        return <Error />
+    };
+
+    return (
+        <main>
+            <Slideshow images={card.pictures} />
+            <section className="infos-card">
+                <section className="titles-tags">
+                    <h1>{card.title}</h1>
+                    <h2>{card.location}</h2>
+                    <Tags host={card.host} tags={card.tags} location={card.location} />
+                </section>
+                <section className="rating-host">
+                    <Rating rating={card.rating} />
+                    <HostInfo name={card.host.name} picture={card.host.picture} />
+                </section>
+            </section>
+            <ul className="collapse-container">
+                <Collapse title="Description" content={card.description} />
+                <Collapse title="Equipements" content={<ul>{card.equipments.map((equipement, index) => <li key={index}>{equipement}</li>)}</ul>} />
+            </ul>
+        </main>
+    );
+}
+
+export default Housing;
