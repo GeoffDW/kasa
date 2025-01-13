dd79e08904756fd670c6cecfc0b1848552c2ef6d
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2025-01-13 11:30:14 +0100

    add links for pages

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import Footer  from './components/footer/footer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/kasa">
       <Nav />
 
       <Routes>
